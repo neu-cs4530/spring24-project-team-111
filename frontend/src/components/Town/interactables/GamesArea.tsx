0efafdd621e28310e4530d085863c675914d1445
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2024-02-28 12:28:24 -0500

    render game scene in popup

--- frontend/src/components/Town/interactables/GamesArea.tsx
+++ frontend/src/components/Town/interactables/GamesArea.tsx
@@ -27,7 +27,7 @@ import ConnectFourArea from './ConnectFour/ConnectFourArea';
 import GameAreaInteractable from './GameArea';
 import Leaderboard from './Leaderboard';
 import TicTacToeArea from './TicTacToe/TicTacToeArea';
-import UndercookedArea from './Undercooked/UndercookedArea';
+import UndercookedMap from './Undercooked/UndercookedMap';
 
 export const INVALID_GAME_AREA_TYPE_MESSAGE = 'Invalid game area type';
 
@@ -102,7 +102,7 @@ function GameArea({ interactableID }: { interactableID: InteractableID }): JSX.E
           ) : gameAreaController.toInteractableAreaModel().type === 'TicTacToeArea' ? (
             <TicTacToeArea interactableID={interactableID} />
           ) : gameAreaController.toInteractableAreaModel().type === 'UndercookedArea' ? (
-            <UndercookedArea interactableID={interactableID} />
+            <UndercookedMap interactableID={interactableID} />
           ) : (
             <>{INVALID_GAME_AREA_TYPE_MESSAGE}</>
           )}
---
+++ frontend/src/components/Town/interactables/Undercooked/UndercookedMap.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import UndercookedAreaController from '../../../../classes/interactable/UndercookedAreaController';
+import { useInteractableAreaController } from '../../../../classes/TownController';
+import { InteractableID } from '../../../../types/CoveyTownSocket';
+import UndercookedGameScene from './UndercookedGameScene';
+
+export default function UndercookedMap({
+  interactableID,
+}: {
+  interactableID: InteractableID;
+}): JSX.Element {
+  const controller = useInteractableAreaController<UndercookedAreaController>(interactableID);
+
+  useEffect(() => {
+    const config = {
+      type: Phaser.AUTO,
+      backgroundColor: '#000000',
+      parent: 'undercooked-map-container',
+      render: { pixelArt: true, powerPreference: 'high-performance' },
+      width: 760,
+      height: 512,
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 0 },
+        },
+      },
+    };
+
+    const game = new Phaser.Game(config);
+    const newGameScene = new UndercookedGameScene(controller);
+    game.scene.add('undercookedBoard', newGameScene, true);
+  }, [controller]);
+
+  return <div id='undercooked-map-container' />;
+}
