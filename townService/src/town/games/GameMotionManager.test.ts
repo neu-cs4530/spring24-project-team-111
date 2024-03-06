6f808e5d77a2108e6082666740e235c177bc7c5f
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2024-03-05 22:44:10 -0500

    added basic tests

--- townService/src/town/games/GameMotionManager.test.ts
+++ townService/src/town/games/GameMotionManager.test.ts
@@ -16,7 +16,7 @@ describe('Motion Manager', () => {
     manager = new GameMotionManager(gameEmitter);
   });
 
-  it('should add a player', () => {
+  it('should add players', () => {
     const player1Socket = mockDeep<GameSocket>();
     const player1 = mockGamePlayer();
     manager.addPlayer(player1, player1Socket);
@@ -28,5 +28,17 @@ describe('Motion Manager', () => {
     manager.addPlayer(player2, player2Socket);
     expect(manager.players.length).toBe(2);
     expect(manager.players.find(p => p === player2)).toBeTruthy();
+  });
+
+  it('should remove players', () => {
+    const player1Socket = mockDeep<GameSocket>();
+    const player1 = mockGamePlayer();
+    manager.addPlayer(player1, player1Socket);
+    expect(manager.players.length).toBe(1);
+    expect(manager.players.find(p => p === player1)).toBeTruthy();
+
+    manager.removePlayer(player1);
+    expect(manager.players.length).toBe(0);
+    expect(manager.players.find(p => p === player1)).toBeFalsy();
   });
 });
