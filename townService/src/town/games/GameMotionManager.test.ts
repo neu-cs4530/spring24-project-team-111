b370cf1e1e4b00edbecfaeacf798e7398f9a7166
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2024-03-05 22:52:40 -0500

    fixed syntax error

--- townService/src/town/games/GameMotionManager.test.ts
+++ townService/src/town/games/GameMotionManager.test.ts
@@ -22,12 +22,6 @@ describe('Motion Manager', () => {
     manager.addPlayer(player1, player1Socket);
     expect(manager.players.length).toBe(1);
     expect(manager.players.find(p => p === player1)).toBeTruthy();
-
-    const player2Socket = mockDeep<GameSocket>();
-    const player2 = mockGamePlayer();
-    manager.addPlayer(player2, player2Socket);
-    expect(manager.players.length).toBe(2);
-    expect(manager.players.find(p => p === player2)).toBeTruthy();
   });
 
   it('should remove players', () => {
@@ -41,4 +35,25 @@ describe('Motion Manager', () => {
     expect(manager.players.length).toBe(0);
     expect(manager.players.find(p => p === player1)).toBeFalsy();
   });
+
+  it('should return a list of players being managed', () => {
+    expect(manager.players.length).toBe(0);
+
+    const player1Socket = mockDeep<GameSocket>();
+    const player2Socket = mockDeep<GameSocket>();
+    const player3Socket = mockDeep<GameSocket>();
+    const player1 = mockGamePlayer();
+    const player2 = mockGamePlayer();
+    const player3 = mockGamePlayer();
+
+    manager.addPlayer(player1, player1Socket);
+    manager.addPlayer(player2, player2Socket);
+    manager.addPlayer(player3, player3Socket);
+    const sortCriteria = (a: GamePlayer, b: GamePlayer) => a.id.localeCompare(b.id);
+    const managedPlayers = [player1, player2, player3].sort(sortCriteria);
+
+    expect(manager.players.sort(sortCriteria)).toEqual(managedPlayers);
+  });
+
+  // it('should ')
 });
