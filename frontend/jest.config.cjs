9db3dfd79a5432e5f3f6de4fd0553c9c55260a52
Stephen Yu <stephenyu1229@gmail.com>
2024-03-12 17:42:10 -0400

    disable gamesarea test for green builds

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -10,6 +10,9 @@ module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'jsdom',
   transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
+  // disable GamesArea test for green builds as per professor's instructions:
+  // it's not possible to import any component that uses Phaser from NodeJS (including in tests)
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/src/components/Town/interactables/GamesArea.test.tsx'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   transform: {
     '^.+\\.tsx?$': [
