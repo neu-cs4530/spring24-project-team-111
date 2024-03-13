87107c198fe57d32b4d7a5eff871c8813da95efb
Stephen Yu <100880386+stephenyuu@users.noreply.github.com>
2024-03-13 18:30:27 -0400

    Frontend work for recipe and timer display (#6)
    * small tweak
    
    * small spacing tweak
    
    * add react icons
    
    * add recipe and timer display
    
    * small spacing tweak
    
    * add dummy implementation of talking to backend for timer

--- frontend/src/components/Town/interactables/Undercooked/UndercookedArea.tsx
+++ frontend/src/components/Town/interactables/Undercooked/UndercookedArea.tsx
@@ -1,10 +1,17 @@
-import { Box, Button, Container, List, ListItem, useToast } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, List, ListItem, Text, useToast } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import UndercookedAreaController from '../../../../classes/interactable/UndercookedAreaController';
+import PlayerController from '../../../../classes/PlayerController';
 import { useInteractableAreaController as getAreaController } from '../../../../classes/TownController';
 import useTownController from '../../../../hooks/useTownController';
-import { InteractableID } from '../../../../types/CoveyTownSocket';
+import { GameStatus, InteractableID } from '../../../../types/CoveyTownSocket';
 import UndercookedBoard from './UndercookedBoard';
+import UndercookedRecipeDisplay from './UndercookedRecipeDisplay';
+import UndercookedTimerDisplay from './UndercookedTimerDisplay';
+
+export type UndercookedGameProps = {
+  gameAreaController: UndercookedAreaController;
+};
 
 export default function UndercookedArea({
   interactableID,
@@ -15,10 +22,14 @@ export default function UndercookedArea({
   const townController = useTownController();
   const gameAreaController = getAreaController<UndercookedAreaController>(interactableID);
 
-  const [joiningGame, setJoiningGame] = useState(false);
-  const [playerOne, setPlayerOne] = useState(gameAreaController.playerOne);
-  const [playerTwo, setPlayerTwo] = useState(gameAreaController.playerTwo);
-  const [gameStatus, setGameStatus] = useState(gameAreaController.status);
+  const [joiningGame, setJoiningGame] = useState<boolean>(false);
+  const [playerOne, setPlayerOne] = useState<PlayerController | undefined>(
+    gameAreaController.playerOne,
+  );
+  const [playerTwo, setPlayerTwo] = useState<PlayerController | undefined>(
+    gameAreaController.playerTwo,
+  );
+  const [gameStatus, setGameStatus] = useState<GameStatus>(gameAreaController.status);
 
   useEffect(() => {
     const updateGameState = () => {
@@ -49,18 +60,45 @@ export default function UndercookedArea({
   };
 
   return (
-    <Container margin={0}>
-      <Box>
-        <b>{gameStatus}</b>
+    <Flex width='768px' mt={2} direction='column' alignItems='center'>
+      <Box width='100%' aria-label='game lobby information'>
+        <Heading as='h1' size='lg' textAlign='center'>
+          Welcome to Undercooked!
+        </Heading>
+        <Heading mt={2} as='h2' size='md' textAlign='center'>
+          Game Status{' '}
+          <Text as='span' fontWeight='normal'>
+            {gameStatus}
+          </Text>
+        </Heading>
+        <Flex mt={2} justifyContent='space-between' alignItems='center'>
+          <Flex gap={2} alignItems='center'>
+            <Heading as='h3' size='sm'>
+              Chefs
+            </Heading>
+            <List aria-label='list of players in the game' display='flex' gap={1}>
+              <ListItem>Chef 1: {playerOne?.userName || '(Chef needed!)'}</ListItem>
+              <ListItem>Chef 2: {playerTwo?.userName || '(Chef needed!)'}</ListItem>
+            </List>
+          </Flex>
+          <Flex gap={2}>
+            <Button size='xs' type='button' onClick={handleJoinGame} disabled={joiningGame}>
+              Join Game
+            </Button>
+            <Button
+              size='xs'
+              type='button'
+              onClick={() => console.log('start game button clicked')}>
+              Start Game
+            </Button>
+          </Flex>
+        </Flex>
       </Box>
-      <Button type='button' onClick={handleJoinGame} disabled={joiningGame}>
-        Join Game
-      </Button>
-      <List aria-label='list of players in the game'>
-        <ListItem>Chef 1: {playerOne?.userName || '(Chef needed!)'}</ListItem>
-        <ListItem>Chef 2: {playerTwo?.userName || '(Chef needed!)'}</ListItem>
-      </List>
+      <Flex mt={2} width='100%' justifyContent='space-between'>
+        <UndercookedRecipeDisplay gameAreaController={gameAreaController} />
+        <UndercookedTimerDisplay gameAreaController={gameAreaController} />
+      </Flex>
       <UndercookedBoard gameAreaController={gameAreaController} />
-    </Container>
+    </Flex>
   );
 }
---
+++ frontend/src/components/Town/interactables/Undercooked/UndercookedTimerDisplay.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Flex, Heading, Icon, Text } from '@chakra-ui/react';
+import { FaRegClock } from 'react-icons/fa';
+import { UndercookedGameProps } from './UndercookedArea';
+
+type Time = number;
+
+const DUMMY_TIME_LEFT: Time = 120;
+
+export default function UndercookedTimerDisplay({
+  gameAreaController,
+}: UndercookedGameProps): JSX.Element {
+  // currently using hard coded value for time left
+  // change implementation to use gameAreaController to get the time left when backend is completed
+  const [timeLeft, setTimeLeft] = useState<Time>(DUMMY_TIME_LEFT);
+
+  return (
+    <Flex gap={2} alignItems='center'>
+      <Icon as={FaRegClock} />
+      <Heading as='h4' size='sm'>
+        Time Left
+      </Heading>
+      <Text>{timeLeft}</Text>
+    </Flex>
+  );
+}
