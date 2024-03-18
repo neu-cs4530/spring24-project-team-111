53e1176535b988b808d9a31ad577368c54d00094
Stephen Yu <100880386+stephenyuu@users.noreply.github.com>
2024-03-18 11:12:26 -0400

    add documentation for the new implementation of the new backend (#12)

--- frontend/src/classes/interactable/UndercookedAreaController.ts
+++ frontend/src/classes/interactable/UndercookedAreaController.ts
@@ -8,10 +8,20 @@ import InteractableAreaController, { BaseInteractableEventMap } from './Interact
 
 export type UndercookedAreaEvents = BaseInteractableEventMap & UndercookedTownEvents;
 
+/**
+ * The UndercookedAreaController class is an adapter class that wraps the UndercookedTownController.
+ * This class was created so the TownController can add an instance of an InteractableAreaController
+ * corresponding to an UndercookedArea in Covey.Town. The actual communication between the frontend
+ * and backend of the Undercooked game will be handled by the UndercookedTownController class.
+ */
 export default class UndercookedAreaController extends InteractableAreaController<
   UndercookedAreaEvents,
   UndercookedAreaModel
 > {
+  /**
+   * The UndercookedTownController that will handle the communication between the Undercooked
+   * game's frontend and backend service.
+   */
   private _undercookedTownController: UndercookedTownController;
 
   constructor(
---
+++ townService/src/town/UndercookedTown.ts
@@ -0,0 +1,8 @@
+/**
+ * The UndercookedTown class implements the logic for an Undercooked game: managing the various
+ * events that can occur (e.g. joining a game, leaving a game, moving around in the undercooked town, etc.)
+ *
+ * The implementation of this class will be similar to Town.ts in the townService directory but tweaked
+ * for Undercooked game rules and mechanics.
+ */
+export default class UndercookedTown {}
