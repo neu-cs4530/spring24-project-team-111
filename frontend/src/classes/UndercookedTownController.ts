e050e1c34d4f991f9b1edc0d7170476be332cce7
Chia Hao Hsu Tai <89400862+chiahaohsutai@users.noreply.github.com>
2024-03-20 12:41:22 -0400

    Refactoring PhaserScenes to support Undercooked game. (#10)
    * refactored TownGameScene with passing tests
    
    * fixed interactable representation in scene so that it matches new walkable scene abstraction
    
    * refactoring create method with passing tests and undercooked scene completed.
    
    * fixed build issues in main

--- frontend/src/classes/UndercookedTownController.ts
+++ frontend/src/classes/UndercookedTownController.ts
@@ -1,19 +1,19 @@
 import EventEmitter from 'events';
 import TypedEmitter from 'typed-emitter';
-import { UndercookedTownSocket } from '../types/CoveyTownSocket';
-import TownController from './TownController';
+import { PlayerLocation, UndercookedTownSocket } from '../types/CoveyTownSocket';
+import Interactable from '../components/Town/Interactable';
+import TownController, { TownEvents } from './TownController';
 import { UndercookedArea as UndercookedAreaModel } from '../types/CoveyTownSocket';
 import { io } from 'socket.io-client';
 import assert from 'assert';
 import { InteractableID } from '../generated/client';
+import PlayerController from './PlayerController';
 
 /**
  * The UndercookedTownController emits these events. Components may subscribe to these events
  * by calling the 'addListener' method on UndercookedTownController
  */
-export type UndercookedTownEvents = {
-  stubEvent: () => void;
-};
+export type UndercookedTownEvents = TownEvents;
 
 /**
  * The (frontend) UndercookedTownController manages the communication between the frontend
@@ -28,15 +28,32 @@ export default class UndercookedTownController extends (EventEmitter as new () =
 
   private _model: UndercookedAreaModel;
 
+  private _ourPlayer?: PlayerController;
+
   private _townController: TownController;
 
   private _id: InteractableID;
+
+  /**
+   * A flag indicating whether the current 2D game is paused, or not. Pausing the game will prevent it from updating,
+   * and will also release any key bindings, allowing all keys to be used for text entry or other purposes.
+   */
+  private _paused = false;
+
+  /**
+   * An event emitter that broadcasts interactable-specific events
+   */
+  private _interactableEmitter = new EventEmitter();
 
   constructor(id: InteractableID, model: UndercookedAreaModel, townController: TownController) {
     super();
     this._model = model;
     this._townController = townController;
     this._id = id;
+  }
+
+  public get paused() {
+    return this._paused;
   }
 
   public async connect() {
@@ -46,5 +63,58 @@ export default class UndercookedTownController extends (EventEmitter as new () =
       this._socket = io(`${url}/undercooked`);
       resolve();
     });
+  }
+
+  // Our player should be the in-game player the client controls.
+  // used in WalkableScene.ts
+  public get ourPlayer() {
+    // this is a stub. Replace with the actual player object.
+    const player = this._townController.ourPlayer;
+    assert(player);
+    return player;
+  }
+
+  public get players() {
+    // this is a stub, it should return the list of players in the game.
+    return this._townController.players;
+  }
+
+  public pause(): void {
+    if (!this._paused) {
+      this._paused = true;
+      this.emit('pause');
+    }
+  }
+
+  public unPause(): void {
+    if (this._paused) {
+      this._paused = false;
+      this.emit('unPause');
+    }
+  }
+
+  /**
+   * Emit a movement event for the current player, updating the state locally and
+   * also notifying the townService that our player moved.
+   *
+   * Note: it is the responsibility of the townService to set the 'interactableID' parameter
+   * of the player's location, and any interactableID set here may be overwritten by the townService
+   *
+   * @param newLocation
+   */
+  public emitMovement(newLocation: PlayerLocation) {
+    this._socket.emit('playerMovement', newLocation);
+    const player = this.ourPlayer;
+    assert(player);
+    player.location = newLocation;
+    this.emit('playerMoved', player);
+  }
+
+  /**
+   * Begin interacting with an interactable object. Emits an event to all listeners.
+   * @param interactedObj
+   */
+  public interact<T extends Interactable>(interactedObj: T) {
+    this._interactableEmitter.emit(interactedObj.getType(), interactedObj);
   }
 }
--- frontend/src/components/Town/interactables/Undercooked/UndercookedGameScene.ts
+++ frontend/src/components/Town/interactables/Undercooked/UndercookedGameScene.ts
@@ -1,29 +1,16 @@
-import Phaser from 'phaser';
+import assert from 'assert';
 import UndercookedAreaController from '../../../../classes/interactable/UndercookedAreaController';
-import PlayerController from '../../../../classes/PlayerController';
+import WalkableScene from '../../WalkableScene';
 
-export default class UndercookedGameScene extends Phaser.Scene {
+export default class UndercookedGameScene extends WalkableScene {
   private _resourcePathPrefix: string;
-
-  private _players: PlayerController[] = [];
 
   public undercookedController: UndercookedAreaController;
 
-  private _map?: Phaser.Tilemaps.Tilemap;
-
   constructor(undercookedAreaController: UndercookedAreaController, resourcePathPrefix = '') {
-    super('UndercookedGameScene');
+    super(undercookedAreaController.undercookedTownController, 'UndercookedGameScene');
     this._resourcePathPrefix = resourcePathPrefix;
     this.undercookedController = undercookedAreaController;
-    this._players = this.undercookedController.players;
-  }
-
-  public get map(): Phaser.Tilemaps.Tilemap {
-    const map = this._map;
-    if (!map) {
-      throw new Error('Cannot access map before it is initialized');
-    }
-    return map;
   }
 
   preload() {
@@ -42,7 +29,7 @@ export default class UndercookedGameScene extends Phaser.Scene {
   }
 
   create() {
-    this._map = this.make.tilemap({ key: 'map' });
+    this.map = this.make.tilemap({ key: 'map' });
 
     // add the tilesets
     const kitchenTileset = this.map.addTilesetImage('kitchen_tiles', '12_Kitchen_32x32');
@@ -53,10 +40,56 @@ export default class UndercookedGameScene extends Phaser.Scene {
     ];
 
     // create the layers
-    this.map.createLayer('Walls', tilesets);
     this.map.createLayer('Floor', tilesets);
-    this.map.createLayer('StaticKitchenSurfaces', tilesets);
-    this.map.createLayer('StaticKitchenProps', tilesets);
-    this.map.createLayer('InteractableKitchen', tilesets);
+    const walls = this.map.createLayer('Walls', tilesets);
+    assert(walls);
+    walls.setCollisionByProperty({ collides: true });
+    const kitchenSurfaces = this.map.createLayer('StaticKitchenSurfaces', tilesets);
+    assert(kitchenSurfaces);
+    kitchenSurfaces.setCollisionByProperty({ collides: true });
+    const staicProps = this.map.createLayer('StaticKitchenProps', tilesets);
+    assert(staicProps);
+    staicProps.setCollisionByProperty({ collides: true });
+    const interactableKitchen = this.map.createLayer('InteractableKitchen', tilesets);
+    assert(interactableKitchen);
+    interactableKitchen.setCollisionByProperty({ collides: true });
+
+    this.createLabels();
+
+    assert(this.input.keyboard);
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
+    this.cursors.push(this.cursorKeys);
+    this.cursors.push(
+      this.input.keyboard.addKeys(
+        {
+          up: Phaser.Input.Keyboard.KeyCodes.W,
+          down: Phaser.Input.Keyboard.KeyCodes.S,
+          left: Phaser.Input.Keyboard.KeyCodes.A,
+          right: Phaser.Input.Keyboard.KeyCodes.D,
+        },
+        false,
+      ) as Phaser.Types.Input.Keyboard.CursorKeys,
+    );
+
+    const sprite = this.createSpawnPoint();
+
+    // Watch the player and worldLayer for collisions, for the duration of the scene:
+    this.collidingLayers.push(walls);
+    this.collidingLayers.push(kitchenSurfaces);
+    this.collidingLayers.push(staicProps);
+    this.collidingLayers.push(interactableKitchen);
+    this.collidingLayers.forEach(layer => this.physics.add.collider(sprite, layer));
+
+    this.interactables = this.getInteractables();
+    this.createAnimationsForSprite();
+    this.addCamera();
+
+    this.ready = true;
+
+    this.lockLabelPositions();
+
+    this.ready = true;
+    this.updatePlayers(this.controller.players);
+    this.initScene();
   }
 }
