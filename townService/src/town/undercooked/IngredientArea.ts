3183dc291a5f09e51deb1e39426c5823efa0b35b
Stephen Yu <stephenyu1229@gmail.com>
2024-03-26 17:29:02 -0400

    fix ingredient interactable area

--- townService/src/town/undercooked/IngredientArea.ts
+++ townService/src/town/undercooked/IngredientArea.ts
@@ -1,12 +1,27 @@
 import Player from '../../lib/Player';
 import {
+  BoundingBox,
   Interactable,
   InteractableCommand,
   InteractableCommandReturnType,
+  TownEmitter,
+  UndercookedIngredient,
 } from '../../types/CoveyTownSocket';
 import InteractableArea from '../InteractableArea';
 
 export default class IngredientArea extends InteractableArea {
+  private _ingredientName: UndercookedIngredient;
+
+  constructor(
+    name: string,
+    coordinates: BoundingBox,
+    townEmitter: TownEmitter,
+    ingredientName: UndercookedIngredient,
+  ) {
+    super(name, coordinates, townEmitter);
+    this._ingredientName = ingredientName;
+  }
+
   public toModel(): Interactable {
     throw new Error('Method not implemented.');
   }
