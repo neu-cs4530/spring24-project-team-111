e812bc7a9d1e2a8289d30f973a0a71079307a810
Stephen Yu <stephenyu1229@gmail.com>
2024-03-28 13:38:41 -0400

    remove undercooked town socket

--- frontend/src/types/CoveyTownSocket.d.ts
+++ frontend/src/types/CoveyTownSocket.d.ts
@@ -7,17 +7,8 @@ besides type declarations could become problematic from a compilation perspectiv
 
 import { Socket } from 'socket.io-client';
 /* eslint-disable import/no-relative-packages */
-import {
-  ClientToServerEvents,
-  ServerToClientEvents,
-  ClientToServerUndercookedEvents,
-  ServerToClientUndercookedEvents,
-} from '../../../shared/types/CoveyTownSocket';
+import { ClientToServerEvents, ServerToClientEvents } from '../../../shared/types/CoveyTownSocket';
 /* eslint-disable import/no-relative-packages */
 export * from '../../../shared/types/CoveyTownSocket';
 
 export type CoveyTownSocket = Socket<ServerToClientEvents, ClientToServerEvents>;
-export type UndercookedTownSocket = Socket<
-  ServerToClientUndercookedEvents,
-  ClientToServerUndercookedEvents
->;
