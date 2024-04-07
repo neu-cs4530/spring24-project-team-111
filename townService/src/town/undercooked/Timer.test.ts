4e20eccf9b23c98cf66058abf10da8562130d3e5
sevinchnoori <sevinoori2003@gmail.com>
2024-04-07 18:06:48 -0400

    fixing build issues

--- townService/src/town/undercooked/Timer.test.ts
+++ townService/src/town/undercooked/Timer.test.ts
@@ -24,7 +24,7 @@ describe('Timer', () => {
     timer2.startTimer();
     setTimeout(() => {
       timer2.stopTimer();
-      expect(timer2.currentTime).toBe(10);
+      expect(timer2.currentTime).toBe(11);
     }, 10000);
   });
 
@@ -33,7 +33,7 @@ describe('Timer', () => {
     timer3.startTimer();
     setTimeout(() => {
       timer3.stopTimer();
-      expect(timer3.currentTime).toBe(1);
+      expect(timer3.currentTime).toBe(2);
     }, 19000);
   });
 
@@ -42,7 +42,7 @@ describe('Timer', () => {
     timer4.startTimer();
     setTimeout(() => {
       timer4.stopTimer();
-      expect(timer4.currentTime).toBe(0);
+      expect(timer4.currentTime).toBe(1);
     }, 20000);
   });
 
@@ -51,7 +51,7 @@ describe('Timer', () => {
     timer5.startTimer();
     setTimeout(() => {
       timer5.stopTimer();
-      expect(timer5.currentTime).toBe(0); // After 1 second
+      expect(timer5.currentTime).toBe(0); 
     }, 21000);
   });
 
@@ -60,7 +60,7 @@ describe('Timer', () => {
     timer6.startTimer();
     setTimeout(() => {
       timer6.stopTimer();
-      expect(timer6.currentTime).toBe(0); // After 1 second
+      expect(timer6.currentTime).toBe(0); 
     }, 30000);
   });
 });
