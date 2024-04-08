f9d861c0515912536d2fe803e976297b0630c706
Stephen Yu <stephenyu1229@gmail.com>
2024-04-07 22:13:48 -0400

    add placeholder for game end toast

--- frontend/src/components/Town/interactables/Undercooked/UndercookedScoreDisplay.tsx
+++ frontend/src/components/Town/interactables/Undercooked/UndercookedScoreDisplay.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Flex, Heading, Icon, Text } from '@chakra-ui/react';
+import { Button, Flex, Heading, Icon, Text, useToast } from '@chakra-ui/react';
 import { FaRegStar } from 'react-icons/fa6';
 import { UndercookedGameProps } from './UndercookedArea';
 
@@ -8,17 +8,37 @@ export default function UndercookedScoreDisplay({
 }: UndercookedGameProps): JSX.Element {
   const [currentScore, setCurrentScore] = useState<number>(undercookedAreaController.currentScore);
 
+  const toast = useToast();
+
+  const testOnGameEnd = () => {
+    toast({
+      title: 'Game over',
+      description: `You scored ${currentScore} points!`,
+      status: 'success',
+    });
+  };
+
   useEffect(() => {
     const updateScore = () => {
       setCurrentScore(undercookedAreaController.currentScore);
     };
 
+    const onGameEnd = () => {
+      toast({
+        title: 'Game over',
+        description: `You scored ${currentScore} points!`,
+        status: 'success',
+      });
+    };
+
     undercookedAreaController.addListener('gameUpdated', updateScore);
+    undercookedAreaController.addListener('gameEnd', onGameEnd);
 
     return () => {
       undercookedAreaController.removeListener('gameUpdated', updateScore);
+      undercookedAreaController.removeListener('gameEnd', onGameEnd);
     };
-  }, [undercookedAreaController]);
+  }, [undercookedAreaController, toast, currentScore]);
 
   return (
     <Flex gap={2} alignItems='center'>
@@ -27,6 +47,9 @@ export default function UndercookedScoreDisplay({
         Score
       </Heading>
       <Text>{currentScore}</Text>
+      <Button size='xs' onClick={testOnGameEnd}>
+        call onGameEnd function
+      </Button>
     </Flex>
   );
 }
